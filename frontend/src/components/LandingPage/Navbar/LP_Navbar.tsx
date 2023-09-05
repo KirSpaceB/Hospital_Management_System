14977e8b4352ab87298d4c950ec89a9250bcc035
KirSpaceB <productivekirk@gmail.com>
2023-09-04 17:45:28 -0700

    fix: navbar fixed

--- frontend/src/components/LandingPage/Navbar/LP_Navbar.tsx
+++ frontend/src/components/LandingPage/Navbar/LP_Navbar.tsx
@@ -1,7 +1,4 @@
 import CompanyLogo from "../../../shared/CompanyLogo"
-import CallToAction from "../../../shared/CallToAction"
-import gloves from '../../../public/gloves.jpg'
-import { textForLandingPage } from '../LP_texts';
 import Items from "./Items";
 
 export default function LP_Navbar() {
@@ -15,22 +12,6 @@ export default function LP_Navbar() {
         <Items/>
       </nav>
 
-      <hr className="bg-black h-[1px] mt-4"></hr>
-
-      <div id="product_pitch" className="w-screen flex flex-row justify-center items-center space-x-28">
-        <div className="flex flex-col justify-start items-start space-y-4">
-          <h1 className="font-extrabold	text-4xl text-black">Just like your staff were here to help!</h1>
-          <p className="text-black text-xl sm:w-[497px]">
-            {textForLandingPage.pTagAboveCallToAction}
-          </p>
-          <div>
-            <CallToAction/>
-          </div>
-        </div>
-        <div className="">
-          <img src={gloves} className="sm:w-[578px] sm:h-[588px] mt-28"></img>
-        </div>
-      </div>
     </div>
   )
 }
